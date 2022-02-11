2170af1e154cd47f605ce34a8de86f907cd17059
SandeepS4 <sampathi.sandeep@gmail.com>
2022-02-11 14:00:30 +0530

    commit

---
+++ example_ci_formula.py
@@ -0,0 +1,11 @@
+from Pakages import own_ci_formula
+
+ci=own_ci_formula.ci()
+addition_value=ci.addition(1,2)
+print(addition_value)
+
+
+# From where u came
+ci=own_ci_formula.ci()
+addition_value=ci.addition(1,2)
+print(addition_value)
